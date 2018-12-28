f8d85d02944fa34e79ddd279dc7765f1d25ef4db
Vaibhav Singh <vaibhavsingh100070@gmail.com>
2018-12-28 14:38:02 +0530

    Project Euler #16: Power digit sum Upload

---
+++ src/Q16.java
@@ -0,0 +1,25 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+/*
+ * 	Project Euler #16: Power digit sum
+ */
+public class Q16 {
+	public static void main(String args[])
+	{
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		while(t-->0)
+		{
+			int n = scanner.nextInt();
+			BigInteger power = BigInteger.valueOf(2);
+			power = power.pow(n);
+			String s = power.toString();
+			BigInteger sum = BigInteger.ZERO;
+			for(int i=0;i<s.length();i++)
+			{
+				sum = sum.add(BigInteger.valueOf(Integer.parseInt(s.charAt(i)+"")));
+			}
+			System.out.println(sum);
+		}
+	}
+}
